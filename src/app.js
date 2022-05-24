b36e17b6b9f43d72c02ab0f1de652fc6533ef213
Fanny <oeilden8@gmail.com>
2022-05-24 14:17:28 +0200

    changed cors for header

--- src/app.js
+++ src/app.js
@@ -14,6 +14,15 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_ORIGIN); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
