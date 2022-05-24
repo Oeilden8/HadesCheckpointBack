21d8057d624acd3473aaee8658e2fccf14883f75
Fanny <oeilden8@gmail.com>
2022-05-24 14:43:03 +0200

    changed front origin to pages

--- src/app.js
+++ src/app.js
@@ -10,18 +10,9 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://oeilden8.github.io",
   })
 );
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.use("/public", express.static("public"));
 
