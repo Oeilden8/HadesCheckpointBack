1e4c4b37fc8595d12772b2796b9181d7cf9f7207
Fanny <oeilden8@gmail.com>
2022-05-24 14:20:48 +0200

    changed cors for header without env

--- src/app.js
+++ src/app.js
@@ -10,18 +10,18 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.CLIENT_ORIGIN,
+    origin: "http://localhost:3000",
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use("/public", express.static("public"));
 
