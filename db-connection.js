32f4950d9df195bc05a73622d3ccdd9ef75576a1
Fanny <oeilden8@gmail.com>
2022-05-24 13:25:40 +0200

    local heroku working

--- db-connection.js
+++ db-connection.js
@@ -2,16 +2,27 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
+// config local mysql
+// let config = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   multipleStatements: true,
+// };
+
+// config cleardb heroku
 let config = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.DB_CLEAR_HOST,
+  port: process.env.DB_CLEAR_PORT,
+  user: process.env.DB_CLEAR_USER,
+  password: process.env.DB_CLEAR_PASS,
+  database: process.env.DB_CLEAR_NAME,
   multipleStatements: true,
 };
 
-const connection = mysql.createConnection(config);
+const connection = mysql.createPool(config);
 
 const query = (...args) => {
   return new Promise((resolve, reject) => {
